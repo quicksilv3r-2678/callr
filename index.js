83ab6df5b1fc614a800cbd4a1753e1555d1febc1
quicksilv3r-2678 <81493812+quicksilv3r-2678@users.noreply.github.com>
2021-04-04 15:55:39 -0700

    ·· 1nit14l c0mm1t ··
    └ b4s3 bu3ld to gen3rate3 an 3x3

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+ ░▒        ╔╔════════════════════════════════════════════════╗╗              ▒█
+ ▓█        ╚╗                                                ╔╝              ▒▓
+ ░▒         ║        ░░░░▒▒▓█▌■CALLR··                       ║               ▒▓
+ ▓█         ║                                                ║               ▒▓
+ ░▒        ╔╝            l4m3 javascript garbage             ╚╗              ░▒
+ ▓█        ╚╗          but still feeds on the weak           ╔╝              ▓█
+ ░▒        ╔╝             like a hungry leopard              ╚╗              ░▒
+ ▓█        ╚╗                                                ╔╝              ▓█
+ ░▒         ║                      ≡quicksilv3r 2021≡        ║               ░▒
+ ▒▓         ║╗                                              ╔║               █▓
+ ▒▓         ╚╝╗╔═╗╔═╦═══╦═════════╦═══╦══════════╦═══╦═╗╔═╗╔╚╝               ▒░
+ ▒▓           ╚╝ ╚╝ ╚═══╝         ╚═══╝          ╚═══╝ ╚╝ ╚╝                 █▓
+ ░▒                                                                          ▒░
+ */
+console.log('≡≡CALLR v0.0.1≡≡')
+console.log('feeding time! Ç···■··')
+console.log('muahahaha')
